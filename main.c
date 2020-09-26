1f3f46153028a70e5d9477238697e3bde0250c8f
Diego Osorio <juan.osorio1599@alumnos.udg.mx>
2020-09-26 11:43:24 -0500

    Se agrego la funcionalidad de la edad

--- main.c
+++ main.c
@@ -2,9 +2,13 @@
 
 int main (){
     char nombre[100];
+    unsigned int edad;
     printf("Hola Mundo\n");
     printf("Nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
     printf("Hola %s", nombre);
+    printf("Edad: ");
+    scanf("%u", edad);
+    printf("Tu edad es: %u", edad);
     return 0;
 }
